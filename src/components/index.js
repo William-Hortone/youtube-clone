3743763599ec24a6809ed3ebdefb8ee5a0aea1fb
William-Hortone <willhortone@gmail.com>
2023-02-02 16:38:28 +0800

    added the content of fed and sidebar

--- src/components/index.js
+++ src/components/index.js
@@ -4,3 +4,6 @@ export { default as SearchFeed } from "./SearchFeed";
 export { default as VideoDetail } from "./VideoDetail";
 export { default as ChannelDetail } from "./ChannelDetail";
 export { default as SearchBar } from "./SearchBar";
+export { default as Videos } from "./Videos";
+export { default as VideoCard } from "./VideoCard";
+export { default as ChannelCard } from "./ChannelCard";
